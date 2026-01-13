574d15665ba7e817538ab81cacbe7fe3bcdea4fc
sylabtv <sylab.studio@gmail.com>
2026-01-13 11:19:47 +0100

    Soumettre le formulaire vous-même après traitement﻿

---
+++ exercice-2/index.js
@@ -0,0 +1,7 @@
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target.elements.identifiant.value);
+  console.log(event.target.elements.password.value);
+  event.target.submit();
+});
